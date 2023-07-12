22f83d5f29cf491ae38dbe00e17ac29549346710
Christian Kongsgaard <chrk@arkitema.com>
2023-07-12 14:43:48 +0200

    fixed import statement

--- src/arkitema_config/__init__.py
+++
@@ -1,6 +0,0 @@
-import tomllib
-from pathlib import Path
-
-pyproject = Path(__file__).parent.parent.parent / "pyproject.toml"
-
-__version__ = tomllib.loads(pyproject.read_text())["tool"]["poetry"]["version"]
--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import tomllib
-from pathlib import Path
-
-from arkitema_config import __version__
-
-
-def test_version():
-    pyproject = Path(__file__).parent.parent / "pyproject.toml"
-    assert __version__ == tomllib.loads(pyproject.read_text())["tool"]["poetry"]["version"]
